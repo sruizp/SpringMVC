fd0ad9c91c759ebc248df5d7b49356af7a284af6
sruizp <sruizp@oesia.com>
2012-09-19 09:55:58 +0200

    First commit

---
+++ SpringMVC_Autocomplete/src/net/viralpatel/springmvc/autocomplete/User.java
@@ -0,0 +1,33 @@
+package net.viralpatel.springmvc.autocomplete;
+ 
+public class User {
+     
+    private String name;
+    private String country;
+    private String technologies;
+    
+    
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getTechnologies() {
+		return technologies;
+	}
+	public void setTechnologies(String technologies) {
+		this.technologies = technologies;
+	}
+ 
+    //Getter and Setter methods
+    
+    
+}
